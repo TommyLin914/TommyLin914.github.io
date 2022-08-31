842ffa616c46b5104172c863dc1e32dc5cef122e
Tommy <tommylin001@gmail.com>
2022-08-31 23:50:49 +0800

    Delete all

--- accompaMe0829/404/js/404script.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector('.container')
-
-window.onmousemove = function(e){
-    let x = e.clientX / 5
-        y = e.clienyY / 5
-    container.style.backgroundPositionX = x + 'px'
-    container.style.backgroundPositionY = y + 'px'
-}
