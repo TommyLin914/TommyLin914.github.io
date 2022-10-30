929eb166740a88e05e7c4118c60a44b8c4c5623d
Tommy <tommylin001@gmail.com>
2022-10-30 10:24:18 +0800

    更新日曆

--- JavaScript/TA/0714/Calendar/calendar.js
+++ JavaScript/TA/0714/Calendar/calendar.js
@@ -1,25 +1,25 @@
-
 let scheduleObj = {
-    date: "2022-07-17",
-    todoDetail:
-        [{
-            title: "掃地",
-            time: "15:00",
-            detail: "詳細內容",
-            location: "家裡",
-            color: "#fa0"
-        }],
+    date: '2022-07-17',
+    todoDetail: [
+        {
+            title: '掃地',
+            time: '15:00',
+            detail: '詳細內容',
+            location: '家裡',
+            color: '#fa0'
+        }
+    ]
 }
-const month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const month_name = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 //#region 宣告
 let btnThisMonth = document.querySelector('.list-date')
+const btnEdit = document.querySelector('.btn-edit')
 let today = new Date()
 let currentDate = today.getDate()
 let currentMonth = today.getMonth()
 let currentYear = today.getFullYear()
 let daysCount = new Date(currentYear, currentMonth + 1, 0).getDate()
-console.log(daysCount);
 
 let firstDay = getFirstDay()
 
@@ -29,13 +29,13 @@ let firstDay = getFirstDay()
 window.onload = function () {
     createCalendar()
     changeMonth(0)
-
 }
 //#endregion
 
 //#region Dom
 const btnSave = document.querySelector('.btn-save')
-const btnMonth = document.querySelector(".btn-display");
+const btnDelete = document.querySelector('.btn-delete')
+const btnMonth = document.querySelector('.btn-display')
 const btnLastMonth = document.querySelector('.btn-left')
 const btnNextMonth = document.querySelector('.btn-right')
 const listDates = document.querySelector('.list-date')
@@ -46,6 +46,9 @@ const btnSet = document.querySelector('.btn-set')
 const modalAdd = document.querySelector('#add-event-modal')
 const modalAddClose = document.querySelector('.btn-close-add')
 const modalEditClose = document.querySelector('.btn-close-edit')
+var addModal = new bootstrap.Modal(document.getElementById('add-event-modal'))
+var showModal = new bootstrap.Modal(document.getElementById('display-event-modal'))
+let inputAll = document.querySelectorAll('input')
 
 const inputTitle = document.querySelector('#set-title')
 const inputTime = document.querySelector('#set-time')
@@ -79,36 +82,43 @@ function createCalendar() {
         if (i == currentDate && currentMonth == today.getMonth() && currentYear == today.getFullYear()) {
             li.classList.add('today')
         }
+        if (i < currentDate) {
+            li.classList.add('pass-date')
+        } else {
+            li.classList.add('allow-date')
+        }
         li.setAttribute('data-id', `${currentYear}-${currentMonth + 1}-${i}`)
         div.innerText = i
         li.append(div)
         if (localStorage.getItem(li.dataset.id) != null) {
             let todoList = JSON.parse(localStorage.getItem(li.dataset.id))
-          
+
             let ulTodo = document.createElement('ul')
             todoList.forEach(function (item, index) {
                 let liTodo = document.createElement('li')
                 liTodo.classList.add('li-todo', 'px-3', 'py-1')
                 liTodo.setAttribute('data-id', `${currentYear}-${currentMonth + 1}-${i}`)
-   
+                liTodo.dataset.index = index
+
                 let divTodo = document.createElement('div')
                 divTodo.classList.add('divColor')
                 divTodo.style.backgroundColor = item.color
                 let pTodo = document.createElement('p')
                 pTodo.classList.add('p-todo')
-                pTodo.setAttribute('style',`border-bottom:2px solid ${item.color}`)
+                pTodo.setAttribute('style', `border-bottom:2px solid ${item.color}`)
                 pTodo.setAttribute('data-id', `${currentYear}-${currentMonth + 1}-${i}`)
-                pTodo.setAttribute('index',index)
                 pTodo.innerText = item.title
+                pTodo.dataset.index = index
                 liTodo.append(divTodo, pTodo)
-
-                ulTodo.classList.add('ul-todo')
-                ulTodo.append(liTodo)
-                li.append(ulTodo)
-                li.onclick = function (event) {
+                liTodo.onclick = function (event) {
                     currentIndex = index
                     displaySchedule(event)
                 }
+
+                ulTodo.classList.add('ul-todo')
+                ulTodo.append(liTodo)
+                ulTodo.dataset.id = `${currentYear}-${currentMonth + 1}-${i}`
+                li.append(ulTodo)
             })
         }
 
@@ -152,14 +162,11 @@ btnNextMonth.onclick = function () {
     setCalendar(1)
 }
 function changeMonth(add) {
-    console.log(currentMonth + ":" + daysCount)
+    console.log(currentMonth + ':' + daysCount)
     currentMonth += add
 
     firstDay = getFirstDay()
     daysCount = new Date(currentYear, currentMonth + 1, 0).getDate()
-    // if (currentMonth == 0) {
-    //     daysCount = new Date(currentYear, 0, 0).getDate()
-    // }
     if (currentMonth == 12) {
         currentMonth = 0
         currentYear += 1
@@ -175,15 +182,21 @@ btnGoBack.onclick = function () {
     currentMonth = today.getMonth()
     currentYear = today.getFullYear()
     setCalendar(0)
-
 }
 
 //#region 新增項目
 
 let dataId
+const allowDate = document.querySelectorAll('.allow-date')
 btnThisMonth.addEventListener('click', function (event) {
-    dataId = event.target.dataset.id
-    clickCheck()
+    // dataId = event.target.dataset.id
+    // clickCheck()
+})
+allowDate.forEach(date => {
+    date.onclick = event => {
+        dataId = event.target.dataset.id
+        clickCheck()
+    }
 })
 function addZero(dataId) {
     let dataArr = dataId.split('-')
@@ -193,14 +206,12 @@ function addZero(dataId) {
 }
 //點日曆新增或檢視
 function clickCheck() {
-    if (dataId == null) { return }
+    if (dataId == null) {
+        return
+    }
     if (dataId.split('-')[1] != currentMonth - 1) {
-        // if (localStorage.getItem(dataId) == null) {
         btnSet.click()
         inputDate.value = addZero(dataId)
-        // } else {
-        //     btnLaunch.click()
-        // }
     }
 }
 //新增項目
@@ -211,9 +222,9 @@ let setTime
 let setDetail
 let setLocation
 let setColor
+let currentIndex = -1
+let currObj
 function setScheduleValues() {
-
-
     let key = dataId
 
     let tempObj = {
@@ -221,20 +232,30 @@ function setScheduleValues() {
         time: inputTime.value,
         detail: inputDetail.value,
         location: inputLoc.value,
-        color: inputColor.value,
+        color: inputColor.value
     }
     let scheduleList = []
-    if (localStorage.getItem(key) == null) {
-        scheduleList.push(tempObj)
+    if (currentIndex == -1) {
+        if (localStorage.getItem(key) == null) {
+            scheduleList.push(tempObj)
+        } else {
+            scheduleList = JSON.parse(localStorage.getItem(key))
+            scheduleList.push(tempObj)
+        }
     } else {
         scheduleList = JSON.parse(localStorage.getItem(key))
-        scheduleList.push(tempObj)
+        scheduleList[currentIndex] = tempObj
     }
 
     localStorage.setItem(key, JSON.stringify(scheduleList))
-
     createCalendar()
     modalAddClose.click()
+    currentIndex = -1
+
+    inputTitle.value = ''
+    inputDetail.value = ''
+    inputLoc.value = ''
+    inputColor.value = '#000'
 }
 function setScheduleInDate() {
     let li = document.createElement('li')
@@ -248,24 +269,50 @@ function setScheduleInDate() {
     div.append(p)
     li.appendChild(div)
 }
-let currentIndex
 
 function displaySchedule(event) {
- 
+    dataId = event.target.dataset.id
     const titleSchedule = document.querySelector('.schedule-title')
     const dateSchedule = document.querySelector('.schedule-date')
     const timeSchedule = document.querySelector('.schedule-time')
     const detailSchedule = document.querySelector('.schedule-detail')
     const locSchedule = document.querySelector('.schedule-location')
     let liDataId = event.target.dataset.id
-    let index =event.target.getAttributeNode('index').value
+    let index = event.target.dataset.index
     let val = JSON.parse(localStorage.getItem(liDataId))
-    titleSchedule.innerText = val[index].title
+
+    currObj = {
+        title: val[index].title,
+        time: val[index].time,
+        detail: val[index].detail,
+        location: val[index].location,
+        color: val[index].color
+    }
+    currentIndex = index
+    titleSchedule.innerText = currObj.title
     dateSchedule.innerText = liDataId
-    timeSchedule.innerText = val[index].time
-    detailSchedule.innerText = val[index].detail
-    locSchedule.innerText = val[index].location
+    timeSchedule.innerText = currObj.time
+    detailSchedule.innerText = currObj.detail
+    locSchedule.innerText = currObj.location
     btnLaunch.click()
     event.stopPropagation()
 }
-//#endregion
+btnEdit.onclick = () => {
+    showModal.toggle()
+    inputTitle.value = currObj.title
+    inputTime.value = currObj.time
+    inputDetail.value = currObj.detail
+    inputLoc.value = currObj.location
+    inputColor.value = currObj.color
+
+    addModal.toggle()
+}
+btnDelete.onclick = () => {
+    if (confirm('確認刪除行程?')) {
+        scheduleList = JSON.parse(localStorage.getItem(dataId))
+        scheduleList.splice(currentIndex, 1)
+        localStorage.setItem(dataId, JSON.stringify(scheduleList))
+        showModal.toggle()
+        createCalendar()
+    }
+}
